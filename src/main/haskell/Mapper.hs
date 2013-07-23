b63740f027f8e95ddd5836baeff9a15d2ce79674
folone <folone@gmail.com>
2013-07-23 11:50:02 +0200

    Separating concerns: stringifying via typeclass.

--- src/main/haskell/Mapper.hs
+++ src/main/haskell/Mapper.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE FlexibleInstances #-}
 import           Data.List
 import           Data.List.Split
 import qualified Data.Map        as M
 
 data Edge a = Edge a a deriving Show
 
+class Stringify a where
+  stringify :: a → String
+
 instance (Eq a) ⇒ Eq (Edge a) where
   Edge x1 y1 == Edge x2 y2 = x1 == x2 && y1 == y2
+
+instance (Show a) ⇒ Stringify ((a, [a])) where
+  stringify (key, val) = (show key) ++ "\t" ++ (intercalate "\t" (map show val))
 
 nlevel = 2
 
@@ -14,7 +21,7 @@ main :: IO ()
 main = interact calculate
 
 calculate :: String → String
-calculate text = processGraph . convert . lines $ text
+calculate text = unlines . processGraph . convert . lines $ text
 
 convert :: [String] → [Edge String]
 convert xs = xs >>= convert' where
@@ -23,8 +30,9 @@ convert xs = xs >>= convert' where
      Edge (head (res x)) (res x !! 1)]
   res        = splitOn "\t"
 
-processGraph :: [Edge String] → String
-processGraph edges = stringify $ M.map (verticesWithin nlevel) mapV where
+processGraph :: [Edge String] → [String]
+processGraph edges = M.elems . M.mapWithKey (curry stringify) $
+                     M.map (verticesWithin nlevel) mapV where
                      verticesWithin n v =
                        let findAdjacent v = edges >>= \x →
                              case x of Edge a b | a == v    → [b]
@@ -40,6 +48,3 @@ processGraph edges = stringify $ M.map (verticesWithin nlevel) mapV where
                        let vertices = nub $
                                       edges >>= \x → case x of Edge a b → [a, b]
                        in M.fromList $ map (\x → (x, x)) vertices
-                     stringify mp =
-                       let keyShow key val = key ++ "\t" ++ intercalate "\t" val
-                       in unlines . M.elems $ M.mapWithKey keyShow mp
