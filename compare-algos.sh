fe467d0b27729d7a52a15d4c127067ae7dd7ca2f
George Leontiev <folone@gmail.com>
2013-08-06 14:47:39 +0200

    Added a very simple facility to compare algorithm output.
    λ Alonzo_Church soundcloud-challenge → λ git master* → ./compare-algos.sh 5                                                   ~/workspace/soundcloud-challenge
    Running for N=1...
    [info] Loading project definition from /home/folone/workspace/soundcloud-challenge/project
    [info] Set current project to SoundCloud (in build file:/home/folone/workspace/soundcloud-challenge/)
    [info] Running info.folone.soundcloud.Algorithmic misc/test.txt 1 /tmp/testres1.txt
    [success] Total time: 1 s, completed Aug 6, 2013 12:43:44 PM
    Output is the same
    Running for N=2...
    [info] Loading project definition from /home/folone/workspace/soundcloud-challenge/project
    [info] Set current project to SoundCloud (in build file:/home/folone/workspace/soundcloud-challenge/)
    [info] Running info.folone.soundcloud.Algorithmic misc/test.txt 2 /tmp/testres1.txt
    [success] Total time: 1 s, completed Aug 6, 2013 12:43:48 PM
    Output is the same
    Running for N=3...
    [info] Loading project definition from /home/folone/workspace/soundcloud-challenge/project
    [info] Set current project to SoundCloud (in build file:/home/folone/workspace/soundcloud-challenge/)
    [info] Running info.folone.soundcloud.Algorithmic misc/test.txt 3 /tmp/testres1.txt
    [success] Total time: 1 s, completed Aug 6, 2013 12:43:52 PM
    Output is the same
    Running for N=4...
    [info] Loading project definition from /home/folone/workspace/soundcloud-challenge/project
    [info] Set current project to SoundCloud (in build file:/home/folone/workspace/soundcloud-challenge/)
    [info] Running info.folone.soundcloud.Algorithmic misc/test.txt 4 /tmp/testres1.txt
    [success] Total time: 1 s, completed Aug 6, 2013 12:43:56 PM
    Output is the same
    Running for N=5...
    [info] Loading project definition from /home/folone/workspace/soundcloud-challenge/project
    [info] Set current project to SoundCloud (in build file:/home/folone/workspace/soundcloud-challenge/)
    [info] Running info.folone.soundcloud.Algorithmic misc/test.txt 5 /tmp/testres1.txt
    [success] Total time: 1 s, completed Aug 6, 2013 12:44:00 PM
    Output is the same

---
+++ compare-algos.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+for i in $(seq 1 $1);
+do
+    echo -e "\033[00;33mRunning for N=$i...\033[0m"
+    ./sbt "run misc/test.txt "$i" /tmp/testres1.txt";
+    cmd="cat misc/test.txt | ./src/main/haskell/MapperAdjacencyList | ./src/main/haskell/ReducerAdjacencyList | "
+    if [ $i -gt 1 ]
+        then
+        x=$(expr $i - 1)
+        for k in $(seq 1 $x); do
+            cmd+="./src/main/haskell/MapperIterate | ./src/main/haskell/ReducerIterate | "
+        done;
+    fi
+    cmd+="./src/main/haskell/ReducerFinal > /tmp/testres2.txt"
+    eval $cmd;
+    if `comm -3 /tmp/testres1.txt /tmp/testres2.txt > /dev/null` ; then
+        echo -e "\033[00;32mOutput is the same\033[0m"
+    else
+        echo -e "\033[00;31mOutput differs!\033[0m"
+        diff /tmp/testres1.txt /tmp/testres2.txt
+    fi
+done
